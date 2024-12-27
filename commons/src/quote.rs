efa11bb6a4dff24581c7f55b136a6b970ea8faac
ligulfzhou <ligulfzhou53@gmail.com>
2024-12-27 12:07:52 +0800

    ...

--- commons/src/quote.rs
+++ commons/src/quote.rs
@@ -1,5 +1,5 @@
 use anchor_client::solana_sdk::pubkey::Pubkey;
-use anyhow::{ensure, Context, Result};
+use anyhow::{anyhow, ensure, Context, Result};
 use lb_clmm::{
     pair_action_access::ActivationType,
     state::{
@@ -157,7 +157,12 @@ pub fn quote_exact_in(
     let mut total_amount_out: u64 = 0;
     let mut total_fee: u64 = 0;
 
+    let mut while_count = 0;
     while amount_in > 0 {
+        dbg!("while..");
+        if while_count>10 {
+            return Err(anyhow!("endless loop"));
+        }
         let active_bin_array_pubkey = get_bin_array_pubkeys_for_swap(
             lb_pair_pubkey,
             &lb_pair,
@@ -174,6 +179,7 @@ pub fn quote_exact_in(
             .context("Active bin array not found")?;
 
         loop {
+            dbg!("loop...");
             if active_bin_array
                 .is_bin_id_within_range(lb_pair.active_id)
                 .is_err()
@@ -209,6 +215,7 @@ pub fn quote_exact_in(
                 lb_pair.advance_active_bin(swap_for_y)?;
             }
         }
+        while_count += 1;
     }
 
     Ok(SwapExactInQuote {
